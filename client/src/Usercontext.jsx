cf86ee6d08a5861b851b190440decb20a3d55034
yatharth srivastava <yatharthsriv18@gmail.com>
2024-06-08 03:22:40 +0530

    changes

--- client/src/Usercontext.jsx
+++ client/src/Usercontext.jsx
@@ -3,36 +3,6 @@
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-// const getCookie = (name) => {
-//   const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-//   return match ? match[2] : null;
-// };
-
-// const LocalStorageContext = createContext();
-
-// // export const useLocalStorage = () => useContext(LocalStorageContext);
-
-// const LocalStorageProvider = ({ children }) => {
-//   const [cookieValue, setCookieValue] = useState('');
-
-//   useEffect(() => {
-//     const cookieName = 'token'; 
-//     const value = getCookie(cookieName);
-
-//     if (value) {
-     
-//       console.log(value)
-//       localStorage.setItem(cookieName, value);
-//       setCookieValue(value);
-//     }
-//   }, []);
-
-//   return (
-//     <LocalStorageContext.Provider value={{ cookieValue, setCookieValue }}>
-//       {children}
-//     </LocalStorageContext.Provider>
-//   );
-// };
 export const Usercontext= createContext()
 
  const Userprovider=({children})=>{
