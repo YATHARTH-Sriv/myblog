e324510afb4cf77d1457bc04fde76c02589c5ef9
yatharth srivastava <yatharthsriv18@gmail.com>
2024-06-08 04:08:58 +0530

    changes

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ const app=express()
 //         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 //         credentials: true, 
 //     }));
-// app.use(cors())
+app.use(cors())
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':"https://d1bfm8yca6vpwt.cloudfront.net"
-    }
-  },
+  // server:{
+  //   proxy:{
+  //     '/api':"https://d1bfm8yca6vpwt.cloudfront.net"
+  //   }
+  // },
   plugins: [react()],
 })
 
