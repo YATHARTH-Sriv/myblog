fdf1a12b6ab3e7d34c62cfd9b50be26b4a0143b5
yatharth srivastava <126802444+YATHARTH-Sriv@users.noreply.github.com>
2024-06-15 10:44:19 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,12 +17,11 @@ connectDB()
 const app=express()
 
 
-// app.use(cors({
-//         origin: ['http://localhost:5173/', 'http://localhost:5173'],
-//         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//         credentials: true, 
-// },
-// ))
+app.use(cors({
+        origin: ['http://localhost:5173/', 'http://localhost:5173'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+},
+))
 
 app.use(cors())
 
